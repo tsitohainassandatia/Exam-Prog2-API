0496ec7018d2d1f4df5b527ba9a043417984d420
tsitohainassandatia <hei.sandatia@gmail.com>
2025-07-31 09:48:07 +0100

    Q2

--- main.py
+++ main.py
@@ -6,7 +6,16 @@ from starlette.responses import Response
 
 app = FastAPI()
 
+#Question 1
 @app.get('/ping')
 def read_ping():
     return Response("pong")
 
+#Question 2
+@app.get('/home')
+def read_home(): 
+    with open("welcome.html", encoding="utf-8") as file:
+        html_content = file.read()
+
+    return Response(content=html_content, status_code=200, 
+media_type="text/html")
